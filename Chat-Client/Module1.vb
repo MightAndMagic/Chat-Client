dfa7a57daf9374a4f0e0a74f504adf089aa71ee6
MightAndMagic <dada.dudu1@web.de>
2011-12-13 19:15:23 +0100

    Fixed client reconnect on server shutdown. Updated to build number 4.

--- Chat-Client/Module1.vb
+++ Chat-Client/Module1.vb
@@ -2,7 +2,7 @@
 Imports System.Net.Sockets
 Imports System.Text
 Module Module1
-    Dim client As New TcpClient
+    Dim client = Nothing
     Dim listenThread As New System.Threading.Thread(AddressOf listen)
     Dim reconnect As Boolean = False
     Sub Main()
@@ -10,7 +10,7 @@ Module Module1
         Dim port As Integer = 27590
         Console.Title = "OpenSchoolChat - Client"
         Console.ForegroundColor = ConsoleColor.White
-        Console.WriteLine("OpenSchoolChat - Client | Alpha, Build 3")
+        Console.WriteLine("OpenSchoolChat - Client | Alpha, Build 4")
         Try
             ip = IPAddress.Parse("127.0.0.1")
         Catch e As Exception
@@ -19,17 +19,28 @@ Module Module1
         End Try
         While True
             connect(ip, port)
-            listenThread.Start()
+            Try
+                listenThread.Start()
+            Catch ex As Exception
+                Try
+                    listenThread.Resume()
+                Catch e As Exception
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("Error starting listener: {0}", e)
+                    Console.ReadLine()
+                    End
+                End Try
+            End Try
+
             While client.Connected
                 send()
             End While
-            Console.ForegroundColor = ConsoleColor.Red
-            Console.WriteLine("Connection closed. Reconnecting...")
         End While
         Console.Read()
     End Sub
     Sub connect(ip, port)
         Try
+            client = New TcpClient
             client.Connect(ip, port)
             Console.ForegroundColor = ConsoleColor.Green
             Console.WriteLine("Connected to server on {0}", client.Client.RemoteEndPoint)
@@ -40,15 +51,19 @@ Module Module1
         End Try
     End Sub
     Sub send()
-            Console.ForegroundColor = ConsoleColor.White
-            Dim nachricht As String = Console.ReadLine()
-            Dim msg(1024) As Byte
-            msg = System.Text.Encoding.ASCII.GetBytes(nachricht)
+        Console.ForegroundColor = ConsoleColor.White
+        Dim nachricht As String = Console.ReadLine()
+        Dim msg(1024) As Byte
+        msg = System.Text.Encoding.ASCII.GetBytes(nachricht)
+        Try
             client.GetStream.Write(msg, 0, msg.Length)
             Console.ForegroundColor = ConsoleColor.Cyan
             Console.WriteLine("{0} um {1} Uhr", client.Client.RemoteEndPoint, TimeOfDay.TimeOfDay)
             Console.ForegroundColor = ConsoleColor.White
             Console.WriteLine(nachricht & vbCrLf)
+        Catch e As Exception
+            client.Close()
+        End Try
     End Sub
     Sub listen()
         Dim nachricht As String = Nothing
@@ -59,8 +74,9 @@ Module Module1
             i = stream.Read(msg, 0, msg.Length)
         Catch ex As Exception
             Console.ForegroundColor = ConsoleColor.Red
-            Console.WriteLine("Connection closed.")
-            End
+            Console.WriteLine("Connection closed. Type anything to reconnect:")
+            Console.ForegroundColor = ConsoleColor.White
+            listenThread.Suspend()
         End Try
         nachricht = System.Text.Encoding.ASCII.GetString(msg)
         Console.ForegroundColor = ConsoleColor.Cyan
